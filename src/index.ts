16a310ccdef73e81e6d34dff7034dbbd32635c39
marcterradas <terradas.marc@gmail.com>
2021-11-14 11:41:01 +0100

    1. build project

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
 require('dotenv').config()
 
-const { exec } = require('child_process');
-
-// env variables
+const util = require('util')
+const exec = util.promisify(require('child_process').exec)
 
 const githubUsername: string | undefined = process.env.GITHUB_USERNAME
 const githubPassword: string | undefined = process.env.GITHUB_PASSWORD
 const projectPath: string | undefined = process.env.PROJECT_PATH
 const githubPagePath: string | undefined = process.env.GITHUBPAGE_PATH
 
-// 1. build project
+const init = async () => {
 
-const buildQuery : string = `npm run build --prefix ${projectPath}`
-exec( buildQuery, (err: object, stdout: object, stderr: object) => {
-    console.log(err, stdout, stderr)
-})
+    const build = async () => {
+
+        const buildQuery : string = `npm run build --prefix ${projectPath}`
+
+        try {
+            const { stdout, stderr } = await exec(buildQuery);
+            console.log('stdout:', stdout);
+            console.log('stderr:', stderr);
+        } catch (e) {
+            console.error(e);
+        }
+
+    }
+
+    build()
+
+}
+
+init()
