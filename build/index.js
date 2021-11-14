8cabb352d578bff35bb3065a17f5b120d0807c1e
marcterradas <terradas.marc@gmail.com>
2021-11-14 11:24:15 +0100

    build project

--- build/index.js
+++ build/index.js
@@ -1,7 +1,13 @@
 "use strict";
 require('dotenv').config();
+var exec = require('child_process').exec;
+// env variables
 var githubUsername = process.env.GITHUB_USERNAME;
 var githubPassword = process.env.GITHUB_PASSWORD;
 var projectPath = process.env.PROJECT_PATH;
 var githubPagePath = process.env.GITHUBPAGE_PATH;
-console.log({ githubUsername: githubUsername, githubPassword: githubPassword, projectPath: projectPath, githubPagePath: githubPagePath });
+// 1. build project
+var buildQuery = "npm run build --prefix " + projectPath;
+exec(buildQuery, function (err, stdout, stderr) {
+    console.log(err, stdout, stderr);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 require('dotenv').config()
 
+const { exec } = require('child_process');
+
+// env variables
+
 const githubUsername: string | undefined = process.env.GITHUB_USERNAME
 const githubPassword: string | undefined = process.env.GITHUB_PASSWORD
 const projectPath: string | undefined = process.env.PROJECT_PATH
 const githubPagePath: string | undefined = process.env.GITHUBPAGE_PATH
 
-console.log({ githubUsername, githubPassword, projectPath, githubPagePath})
+// 1. build project
+
+const buildQuery : string = `npm run build --prefix ${projectPath}`
+exec( buildQuery, (err: object, stdout: object, stderr: object) => {
+    console.log(err, stdout, stderr)
+})
