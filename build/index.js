b4f9cf150055fe1b3f0d326b868578428a134b71
marcterradas <terradas.marc@gmail.com>
2021-11-14 10:57:30 +0100

    get dotenv variables

--- build/index.js
+++ build/index.js
@@ -1,3 +1,7 @@
 "use strict";
 require('dotenv').config();
-console.log(process.env.GITHUB_USERNAME);
+var githubUsername = process.env.GITHUB_USERNAME;
+var githubPassword = process.env.GITHUB_PASSWORD;
+var projectPath = process.env.PROJECT_PATH;
+var githubPagePath = process.env.GITHUBPAGE_PATH;
+console.log({ githubUsername: githubUsername, githubPassword: githubPassword, projectPath: projectPath, githubPagePath: githubPagePath });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 require('dotenv').config()
 
-console.log(process.env.GITHUB_USERNAME)
+const githubUsername: string | undefined = process.env.GITHUB_USERNAME
+const githubPassword: string | undefined = process.env.GITHUB_PASSWORD
+const projectPath: string | undefined = process.env.PROJECT_PATH
+const githubPagePath: string | undefined = process.env.GITHUBPAGE_PATH
+
+console.log({ githubUsername, githubPassword, projectPath, githubPagePath})
