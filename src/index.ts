f5257c5920f9e64c8d7abad095305583b2c95578
marcterradas <terradas.marc@gmail.com>
2021-11-15 19:50:20 +0100

    add prettier file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const { performance } = require('perf_hooks')
-const {promisify} = require('util')
+const { promisify } = require('util')
 const exec = promisify(require('child_process').exec)
 const question = require('readline-sync').question
 
@@ -19,7 +19,6 @@ interface Response {
  * @returns { Promise<boolean> } false if any error, true if all okay.
  */
 const init = async (): Promise<boolean> => {
-
     // ask user about commit message
     const commitMessage: string = await askCommitMessage()
 
@@ -29,7 +28,7 @@ const init = async (): Promise<boolean> => {
     const startBuildTime: number = performance.now()
     const buildResponse: Response = await build()
 
-    if(!buildResponse.status){
+    if (!buildResponse.status) {
         console.error(buildResponse)
         return false
     }
@@ -40,8 +39,8 @@ const init = async (): Promise<boolean> => {
     // clean github page folder
     const startCleanFolderTime: number = performance.now()
     const cleanFolderResponse: Response = await cleanFolder()
- 
-    if(!cleanFolderResponse.status){
+
+    if (!cleanFolderResponse.status) {
         console.error(cleanFolderResponse)
         return false
     }
@@ -53,7 +52,7 @@ const init = async (): Promise<boolean> => {
     const startMoveDistFolderTime: number = performance.now()
     const moveDistFolderResponse: Response = await moveDistFolder()
 
-    if(!moveDistFolderResponse.status){
+    if (!moveDistFolderResponse.status) {
         console.error(moveDistFolderResponse)
         return false
     }
@@ -63,9 +62,9 @@ const init = async (): Promise<boolean> => {
 
     // push to github
     const startPushToGithub: number = performance.now()
-    const gitPushResponse: Response = await gitPush( commitMessage )
+    const gitPushResponse: Response = await gitPush(commitMessage)
 
-    if(!gitPushResponse.status){
+    if (!gitPushResponse.status) {
         console.error(gitPushResponse)
         return false
     }
@@ -77,7 +76,6 @@ const init = async (): Promise<boolean> => {
     console.log(`\u2705 deploy completed! total time: ${endTime.toFixed(2)} seconds`)
 
     return true
-
 }
 
 /**
@@ -85,15 +83,13 @@ const init = async (): Promise<boolean> => {
  * @returns { String }
  */
 const askCommitMessage = (): string => {
-
     const answer: string = question('commit message: \n')
 
-    if( answer.length == 0 ){
+    if (answer.length == 0) {
         askCommitMessage()
     }
 
     return answer
-
 }
 
 /**
@@ -101,27 +97,21 @@ const askCommitMessage = (): string => {
  * @returns { Promise } Promise with object of type Response.
  */
 const build = async (): Promise<Response> => {
-
-    if(!projectPath) {
-        const response : Response = { code: 1, status: false, msg: 'project path is required' }
+    if (!projectPath) {
+        const response: Response = { code: 1, status: false, msg: 'project path is required' }
         return response
     }
 
-    const query : string = `npm run build --prefix ${projectPath}`
+    const query: string = `npm run build --prefix ${projectPath}`
 
     try {
-
         const { stdout, stderr } = await exec(query)
-        const response : Response = { code: 2, status: true, msg: 'build completed' }
+        const response: Response = { code: 2, status: true, msg: 'build completed' }
         return response
-
     } catch (error: any) {
-
-        const response : Response = { code: 3, status: false, msg: error }
+        const response: Response = { code: 3, status: false, msg: error }
         return response
-
     }
-
 }
 
 /**
@@ -129,27 +119,21 @@ const build = async (): Promise<Response> => {
  * @returns { Promise } Promise with object of type Response.
  */
 const cleanFolder = async (): Promise<Response> => {
-
-    if(!githubPagePath){
-        const response : Response = { code: 1, status: false, msg: 'github page path is required' }
+    if (!githubPagePath) {
+        const response: Response = { code: 1, status: false, msg: 'github page path is required' }
         return response
     }
 
-    const query : string = `rm -rf ${githubPagePath}/*`
+    const query: string = `rm -rf ${githubPagePath}/*`
 
     try {
-
         const { stdout, stderr } = await exec(query)
-        const response : Response = { code: 2, status: true, msg: 'clean destiny folder completed' }
+        const response: Response = { code: 2, status: true, msg: 'clean destiny folder completed' }
         return response
-
     } catch (error: any) {
-
-        const response : Response = { code: 3, status: false, msg: error }
+        const response: Response = { code: 3, status: false, msg: error }
         return response
-
     }
-
 }
 
 /**
@@ -157,51 +141,39 @@ const cleanFolder = async (): Promise<Response> => {
  * @returns { Promise } Promise with object of type Response.
  */
 const moveDistFolder = async (): Promise<Response> => {
-    
     // here is not required to validate two paths becouse they are validated in previous methods
-    const query : string = `cp -a ${projectPath}/dist/. ${githubPagePath}`
+    const query: string = `cp -a ${projectPath}/dist/. ${githubPagePath}`
 
     try {
-
         const { stdout, stderr } = await exec(query)
-        const response : Response = { code: 1, status: true, msg: 'move dist folder to destiny folder completed' }
+        const response: Response = { code: 1, status: true, msg: 'move dist folder to destiny folder completed' }
         return response
-
     } catch (error: any) {
-
-        const response : Response = { code: 2, status: false, msg: error }
+        const response: Response = { code: 2, status: false, msg: error }
         return response
-
     }
-
 }
 
 /**
  * pull changes to github
  * @returns { Promise } Promise with object of type Response.
  */
-const gitPush = async ( commitMessage: string ): Promise<Response> => {
-
-    const query1 : string = `git -C ${githubPagePath} add .`
-    const query2 : string = `git -C ${githubPagePath} commit -m "${commitMessage}"`
-    const query3 : string = `git -C ${githubPagePath} push`
+const gitPush = async (commitMessage: string): Promise<Response> => {
+    const query1: string = `git -C ${githubPagePath} add .`
+    const query2: string = `git -C ${githubPagePath} commit -m "${commitMessage}"`
+    const query3: string = `git -C ${githubPagePath} push`
 
     try {
-
         const { stdout1, stderr1 } = await exec(query1)
         const { stdout2, stderr2 } = await exec(query2)
         const { stdout3, stderr3 } = await exec(query3)
 
-        const response : Response = { code: 1, status: true, msg: 'git push completed!' }
+        const response: Response = { code: 1, status: true, msg: 'git push completed!' }
         return response
-
-    } catch( error: any ){
-
-        const response : Response = { code: 2, status: false, msg: error }
+    } catch (error: any) {
+        const response: Response = { code: 2, status: false, msg: error }
         return response
-
     }
-
 }
 
 init()
