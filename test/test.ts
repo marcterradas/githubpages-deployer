31fae496ac14cf06771bbaeb57ac92decdc80af5
marcterradas <terradas.marc@gmail.com>
2021-11-20 15:17:00 +0100

    add build test

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,20 @@
-import { askCommitMessage, build, cleanFolder, moveDistFolder, gitPush } from '../src/functions'
+require('dotenv').config()
 
-test('testAskCommitMessage', async () => {
+import { askCommitMessage, build, cleanFolder, moveDistFolder, gitPush } from '../src/functions'
+import { Response } from '../src/interfaces'
+
+const projectPath: string | undefined = process.env.PROJECT_PATH
+const githubPagePath: string | undefined = process.env.GITHUBPAGE_PATH
+
+test('askCommitMessage', async () => {
     const commitMessage: string = await askCommitMessage()
     expect(commitMessage.length).not.toEqual(0)
+})
+
+test('build', async () => {
+    const response1: Response = await build(undefined)
+    expect(response1.status).toBe(false)
+
+    const response2: Response = await build(projectPath)
+    expect(response2.status).toBe(true)
 })
